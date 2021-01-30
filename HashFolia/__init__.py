e4ab3377cc0e3a243e450047d270ca424cb7e31e
volooptaz K <volooptaz@riseup.net>
2021-01-30 01:40:48 -0300

    Retorna também listas de palavras e arcanos; adiciona bootstrap no template

--- HashFolia/__init__.py
+++ HashFolia/__init__.py
@@ -5,6 +5,9 @@ from io import BytesIO
 import sys
 import os
 
+import random
+from subprocess import Popen, PIPE 
+
 from flask import Flask
 from flask import render_template, send_file
 
@@ -13,10 +16,47 @@ from robohash import Robohash
 
 from .logogram import logogram
 
+from .deck import deck
+
 app = Flask(__name__)
 
+NUM_PALAVRAS = (3, 3, 3, 3, 3, 3, 3, 3)
+WORDLIST = 'pt-br'
 
-def hashfolia(seed):
+def palabra():
+    def comando_diceware(rodadas):
+        comando = ['diceware', '-n', '1', '-r', 'realdice']
+        if WORDLIST:
+            comando.extend(['-w', WORDLIST])
+        cli_input = rodadas.encode('utf-8')
+        p = Popen(comando, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+        stdout_data = p.communicate(input=cli_input)[0][99:-1]
+        return stdout_data.decode('utf-8')
+
+    rs = [random.choice(range(1, 7)) for i in range(5)]
+
+    rodadas_cli = ', '.join([str(n) for n in rs])
+    palabra = comando_diceware(rodadas_cli)
+
+    return palabra
+
+
+def hashfolia_taro(seed):
+    arcano_maior = random.choice(deck['major'])
+    arcano_menor = random.choice(deck['minor'])
+    return (arcano_maior, arcano_menor)
+
+def hashfolia_palavras(seed):
+    random.seed(seed)
+    palavras = []
+    for group_num in NUM_PALAVRAS:
+        resultado = [palabra() for i in range(group_num)]
+        resultado_str = ''.join(resultado)
+        palavras.append(resultado_str)
+    return palavras
+
+
+def hashfolia_img(seed):
     vh = vizhash.Vizhash(seed, 32, 16)
     vizhash_img = vh.identicon()
 
@@ -52,7 +92,7 @@ def index():
 
 @app.route('/<seed>.png')
 def get_image(seed):
-    img = hashfolia(seed)
+    img = hashfolia_img(seed)
     filename = '/tmp/hashfolia-' + seed + '.png'
     img.save(filename)
     #buffered = BytesIO()
@@ -61,8 +101,8 @@ def get_image(seed):
     return send_file(filename, mimetype='image/png')
 
 @app.route('/<seed>')
-def get_hashfolia(seed):
-    return render_template('hashfolia.html', seed=seed)
+def get_hashfolia_img(seed):
+    return render_template('hashfolia.html', seed=seed, palavras=hashfolia_palavras(seed), taro=hashfolia_taro(seed))
 
 if __name__ == "__main__":
     app.run()
